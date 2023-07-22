f95b200edcb5e8b8ece4776c9f5735b5ad092007
Aurilia0617 <aurilia.ict@gmail.com>
2023-07-22 17:29:39 +0800

    remove database

--- potential_init.sh
+++ potential_init.sh
@@ -25,21 +25,4 @@ else
   go version
 fi
 
-# 检查系统是否已经安装MongoDB
-if ! command -v mongod >/dev/null 2>&1; then
-  # 如果未安装，则安装MongoDB
-  echo "Installing MongoDB..."
-  sudo apt-get install gnupg
-  curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
-   sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg \
-   --dearmor
-  echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
-  sudo apt-get update
-  sudo apt-get install -y mongodb-org
-else
-  # 如果已安装，则输出MongoDB版本信息
-  echo "MongoDB is already installed. Version:"
-  mongod --version
-fi
-
 echo "初始化完成"
